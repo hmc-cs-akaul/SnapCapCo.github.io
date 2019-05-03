3c3e8f9d330517f3b949257a2730166ace1ec40d
Anisha Kaul <35541715+hmc-cs-akaul@users.noreply.github.com>
2019-05-03 12:06:27 -0700

    Update routes.py

--- cs121/routes.py
+++ cs121/routes.py
@@ -26,15 +26,22 @@ def upload_file():
             filename = secure_filename(file.filename)
             save_to=(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file.save(save_to)
+            
+            #load in emotion detection model, load result into pred_class
             pred_class=predictor.model_predict(save_to, '/home/ubuntu/cs121/app')
-            pred_array = [pred_class] + getSongData(pred_class)
+            
+            #create array of generated info
+            pred_array = getSongData(pred_class)
             pred_emotion = pred_array[0]
             pred_caption = pred_array[1]
             pred_song = pred_array[2]
             pred_artist = pred_array[3]
+            
+            #send generated info to displayResult
             return render_template('displayResult.html', filename=filename, prediction=pred_emotion, caption=pred_caption, song=pred_song, artist=pred_artist)
     return render_template('index.html')
 
+#get song data from matching song database csv
 def generate_caption(pred_class):
     if pred_class == 'happy':
         return getSongData('/home/ubuntu/cs121/app/databases/happysongs.csv')
@@ -45,18 +52,23 @@ def generate_caption(pred_class):
     if pred_class == 'neutral':
        return getSongData('/home/ubuntu/cs121/app/databases/neutralsongs.csv')
 
+#parse appropriate song database, return array of generated info
 def getSongData(fileName):
     with open(fileName, mode='r') as csvFile:
-        return fileName
         reader = csv.reader(csvFile, delimiter=',')
         csvlist = [r for r in reader]
         row_count = len(csvlist)
         
+        #generate random value in range of number of songs in database
         randValue = random.randint(0,row_count+1)
+        
+        #pull random row from database and store info
         title = csvlist[randValue][0]
         artist = csvlist[randValue][1]
         lyric = csvlist[randValue][2]
-        songArray = [lyric] + [title] + [artist]
+        
+        #create and return array of generated caption info
+        songArray = [lyric, title, artist]
         return songArray
 
 # allowed image types
