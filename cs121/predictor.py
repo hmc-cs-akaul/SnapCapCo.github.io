292e264fa9595ef8a5259b0a94a7246b327c7777
Lindsey Cleary <lcleary5@gmail.com>
2019-05-03 11:49:36 -0700

    commenting predictor

--- cs121/predictor.py
+++ cs121/predictor.py
@@ -16,9 +16,10 @@ from werkzeug.utils import secure_filename
 
 
 
-#load model
+#load the model
 def load_model(model_path):
     path = Path(model_path)
+    # the classes for our model at the correct index it returns
     classes = ['anger', 'happy','neutral', 'sad']
     data2 = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
     learn = create_cnn(data2, models.resnet34)
@@ -31,5 +32,7 @@ def model_predict(img_path, model_path):
     """
     learn = load_model(model_path)
     img = open_image(img_path)
+    # get the outputs from the model
     pred_class,pred_idx,outputs = learn.predict(img)
+    # return the classification the model returns
     return pred_class
